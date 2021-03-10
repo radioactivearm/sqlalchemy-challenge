c94775de2536e3c6c030ee96c308eb6eb40d5b8b
arm <andy.mcrae21@gmail.com>
2021-03-10 14:47:42 -0600

    finished Flask

--- app.py
+++ app.py
@@ -89,6 +89,101 @@ def get_station():
     return jsonify(station_list)
 
 
+# tobs query
+@app.route("/api/v1.0/tobs")
+def get_tobs():
+    session = Session(engine)
+
+    "Returns list of Temperatures over last year from most active station"
+    # list of stations in desceding order
+    stations_desc = session.query(Measurement.station, func.count(Measurement.tobs)).\
+        group_by(Measurement.station).\
+        order_by(func.count(Measurement.tobs).desc()).all()
+
+    # most active station
+    station_one = stations_desc[0][0]
+
+    # measure a year ago
+    a_year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    # temps and dates over last year from most active station
+    temp_list = session.query(Measurement.date, Measurement.tobs).filter(Measurement.station == station_one).\
+        filter(func.strftime('%Y-%m-%d', Measurement.date) > a_year_ago).all()
+
+    # temps in list with out tuples
+    new_temp_list = [x[1] for x in temp_list]
+
+    # ending session
+    session.close()
+
+    return jsonify(new_temp_list)
+
+
+# start date
+@app.route("/api/v1.0/<start>")
+def begin(start):
+    session = Session(engine)
+
+    """Given a start date, returns min, max and average temps"""
+
+    # calculating min temp in range using query
+    # the [0][0] at the end is to pull it out of list and tuple
+    tmin = session.query(func.min(Measurement.tobs)).\
+        filter(func.strftime('%Y-%m-%d', Measurement.date) >= func.strftime('%Y-%m-%d', start)).all()[0][0]
+
+    # calculating avg temp in range using query
+    # the [0][0] at the end is to pull it out of list and tuple
+    tavg = session.query(func.avg(Measurement.tobs)).\
+        filter(func.strftime('%Y-%m-%d', Measurement.date) >= func.strftime('%Y-%m-%d', start)).all()[0][0]
+
+    # calculating max temp in range using query
+    # the [0][0] at the end is to pull it out of list and tuple
+    tmax = session.query(func.max(Measurement.tobs)).\
+        filter(func.strftime('%Y-%m-%d', Measurement.date) >= func.strftime('%Y-%m-%d', start)).all()[0][0]
+
+    # making list that will be jsonified
+    t_list = [tmin, tavg, tmax]
+
+    # closing session
+    session.close()
+
+    return jsonify(t_list)
+
+
+# start and end
+@app.route("/api/v1.0/<start>/<end>")
+def end(start, end):
+    session = Session(engine)
+
+    """Given a start and an end date, returns min, max and average temps"""
+
+    # calculating min temp in range using query
+    # the [0][0] at the end is to pull it out of list and tuple
+    tmin = session.query(func.min(Measurement.tobs)).\
+        filter(func.strftime('%Y-%m-%d', Measurement.date) >= func.strftime('%Y-%m-%d', start)).\
+        filter(func.strftime('%Y-%m-%d', Measurement.date) <= func.strftime('%Y-%m-%d', end)).all()[0][0]
+
+    # calculating avg temp in range using query
+    # the [0][0] at the end is to pull it out of list and tuple
+    tavg = session.query(func.avg(Measurement.tobs)).\
+        filter(func.strftime('%Y-%m-%d', Measurement.date) >= func.strftime('%Y-%m-%d', start)).\
+        filter(func.strftime('%Y-%m-%d', Measurement.date) <= func.strftime('%Y-%m-%d', end)).all()[0][0]
+
+    # calculating max temp in range using query
+    # the [0][0] at the end is to pull it out of list and tuple
+    tmax = session.query(func.max(Measurement.tobs)).\
+        filter(func.strftime('%Y-%m-%d', Measurement.date) >= func.strftime('%Y-%m-%d', start)).\
+        filter(func.strftime('%Y-%m-%d', Measurement.date) <= func.strftime('%Y-%m-%d', end)).all()[0][0]
+
+    # making a list to return jsonifyed
+    t_list = [tmin, tavg, tmax]
+
+    # closing session
+    session.close()
+
+    return jsonify(t_list)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
